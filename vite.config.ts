f7bffeeecb043c6d0b723cdc4dcc705953972208
Davut <davut@Davut-MacBook-Pro.local>
2025-05-27 12:30:41 +0300

    core: build complated

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+const repoName = "isiksoft-fe-devcase-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: `/${repoName}/`,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
   },
 });
